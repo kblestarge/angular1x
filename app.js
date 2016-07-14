a6830591a891a833c354ce27931a18a08e86230f
kevbot <kblestarge@gmail.com>
2016-07-14 14:46:54 -0600

    first draft of FIZZBUZZ finished. waiting to code review with Sam

--- app.js
+++ app.js
@@ -1,83 +1,29 @@
 (function(){
 	var app = angular.module('fizzbuzz', [ ]);
-	var info = {
-		fizz : 'hi i\'m fizz',
-		buzz : 'hi i\'m buzz'
-	};
 
-	app.controller('FizzbuzzController', function(){
-		this.stuff = info;
-		this.input = 100;
-		this.getNumber = function(num) {
-		    return new Array(num);   
-		}
+	//some services to share data between controllers
+	app.factory('AllRules', function(){
+	  return [];
+	});
+	app.factory('CountTo', function(){
+	  return {
+	  	count : 100
+	  };
 	});
 
-	app.controller('AddRuleController', function(){
-		var self = this;
-
-		self.numbers = [1,2,3,4,5,6,7,8,9,10];
-		self.allRules = [];
-		self.showNumberBtns = false;
-		self.showOperatorBtns = false;
-		self.showFinishingBtns = false;
-		
-		self.addRule = function(){
-			self.allRules.push(new self.NewRule());
-			//self.generateOutput(self.allRules[self.allRules.length-1]);
-			//self.allRules[self.allRules.length-1].humanOutput = self.allRules[self.allRules.length-1].output;
-			self.showNumberBtns = true;
-			self.showNumberBtns = true;
-			console.log('allRules: ',self.allRules);
-		};
-
-		self.addNumber = function(num){
-			self.allRules[self.allRules.length-1].output += num;
-			//self.generateOutput(self.allRules[self.allRules.length-1]);
-			//self.allRules[self.allRules.length-1].humanOutput = 'if number is divisible by '+self.allRules[self.allRules.length-1].output+' then print ';
-			self.showNumberBtns = false;
-			self.showOperatorBtns = true;
-			// console.log('addNumber');
-		};
-
-		self.setOperator = function(operator){
-			self.allRules[self.allRules.length-1].output += ' '+operator+' ';
-			//self.generateOutput(self.allRules[self.allRules.length-1]);
-			self.showNumberBtns = true;
-			self.showOperatorBtns = false;	
-		}
-
-		self.print = function(){
-			self.showOperatorBtns = false;
-			self.showFinishingBtns = true;
-		}
-
-		self.appendRule = function(){
-			var ruleContainer = document.getElementById('set-rules');
-			var node = document.createElement("LI");
-			//self.allRules[self.allRules.length-1].humanOutput = self.allRules[self.allRules.length-1].output+' then print ';
-			var textnode = document.createTextNode(self.generateOutput(self.allRules[self.allRules.length-1]));
-			node.appendChild(textnode);
-			ruleContainer.appendChild(node);
-			self.showFinishingBtns = false;
-			//console.log('Logic Statment: ',self.allRules[self.allRules.length-1].displayLogicStatment());
-		}
-
-		self.generateOutput = function(rule){
-			return 'if number is divisible by '+rule.output+' then print '+rule.word;
-		}
-
-		self.displayOutput = function(){
+	app.controller('ResultsController', function(AllRules, CountTo){
+		this.allRules = AllRules;
+		this.countTo = CountTo;
+		this.displayOutput = function(){
 			console.log('output!');
 			var finalOutput = '';
-			for(var i=1; i<=100; i++){
+			for(var i=1; i<=this.countTo.count; i++){
 				var nothingToPrint = true;
-				for(var j=0; j<self.allRules.length && nothingToPrint; j++){
-					//self.allRules[j].output.;
-					if(eval(self.allRules[j].createLogicStatment(i))){
+				for(var j=0; j<this.allRules.length && nothingToPrint; j++){
+					if(eval(this.allRules[j].ifConditions.replace(/NUM/g, i))){ // /NUM/g is a regular expression that will replace ALL of the "NUM"s
 						nothingToPrint = false;
-						//use word
-						finalOutput += '<p>'+self.allRules[j].word+'</p>';
+						//use output
+						finalOutput += '<p class="special">'+this.allRules[j].output+'</p>';
 					}
 				}
 				if(nothingToPrint){
@@ -88,20 +34,79 @@
 			document.getElementById('output').innerHTML = finalOutput;
 		}
 
-		//newRule Class
+	});
+
+	app.controller('AddRuleController', function(AllRules, CountTo){
+		var self = this;
+
+		self.numbers = [1,2,3,4,5,6,7,8,9,10];
+		self.countTo = CountTo;
+		self.allRules = AllRules;
+		self.showNumberBtns = false;
+		self.showOperatorBtns = false;
+		self.showFinishingBtns = false;
+
+		self.getCurrentRule = function(){
+			return self.allRules.length-1;
+		};
+		
+		self.addRule = function(){
+			self.allRules.push(new self.NewRule());
+			self.showNumberBtns = true;
+			self.showNumberBtns = true;
+			console.log('allRules: ',self.allRules);
+		};
+
+		self.addNumber = function(num){
+			self.allRules[self.getCurrentRule()].conditions += num;
+			self.showNumberBtns = false;
+			self.showOperatorBtns = true;
+			// console.log('addNumber');
+		};
+
+		self.setOperator = function(operator){
+			self.allRules[self.getCurrentRule()].conditions += ' '+operator+' ';
+			self.showNumberBtns = true;
+			self.showOperatorBtns = false;	
+		}
+
+		self.print = function(){
+			self.showOperatorBtns = false;
+			self.showFinishingBtns = true;
+		}
+
+		self.saveRule = function(){
+			var ruleContainer = document.getElementById('set-rules');
+			var node = document.createElement("LI");
+			var textnode = document.createTextNode(self.generateOutput(self.allRules[self.getCurrentRule()]));
+			node.appendChild(textnode);
+			ruleContainer.appendChild(node);
+			self.allRules[self.getCurrentRule()].setIfConditions();
+			// console.log('ifConditions: ',self.allRules[self.getCurrentRule()].ifConditions);
+			self.showFinishingBtns = false;
+		}
+
+		self.generateOutput = function(rule){
+			return 'if number is divisible by '+rule.conditions+' then print '+rule.output;
+		}
+
+		self.clearRules = function(){
+			self.allRules.length = 0;
+			document.getElementById('set-rules').innerHTML = '';
+		}
+
+		//NewRule Class
 		self.NewRule = function(){
+			this.conditions = '';
 			this.output = '';
-			this.word = '';
-			// this.humanOutput = '';
-			//this.logicStatment
-			this.createLogicStatment = function(incNum){
-				var res = this.output.split(" ");
+			this.ifConditions = '';
+			this.setIfConditions = function(){
+				var res = this.conditions.split(" ");
 				var logicStatement = '';
-				console.log('res: '+res);
 				for(var i=0; i<res.length; i++){
 					if(i%2 == 0){
-						//number
-						logicStatement += incNum+'%'+res[i]+' == 0';
+						//numbers
+						logicStatement += 'NUM%'+res[i]+' == 0';
 					}else{
 						//operator
 						if(res[i] == 'and'){
@@ -109,12 +114,10 @@
 						}else{ //or
 							logicStatement += ' || ';
 						}
-						
 					}
 				}
-				return logicStatement;
-				//this.logicStatement = logicStatement;
-			}
+				this.ifConditions = logicStatement;
+			};
 		};
 	});
 })();
